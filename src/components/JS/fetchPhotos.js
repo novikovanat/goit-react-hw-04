7001dff267164800ca818731135064eaf5936db8
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-04-23 20:08:32 +0200

    axios added

--- src/components/JS/fetchPhotos.js
+++ src/components/JS/fetchPhotos.js
@@ -1,15 +1,17 @@
-export default function fetchPhotos(searchPrase, page = 1) {
-  const searchParams = new URLSearchParams({
-    client_id: "BUqEmomTG7T1vLBnVUi4F4sAZAHb-r9RCsxNJ7SWWvI",
-    auto: "format",
-    page: page,
-    per_page: 30,
-    query: searchPrase,
-  });
+import axios from "axios";
 
-  return fetch(`https://api.unsplash.com/search/photos?${searchParams}`, {
+export default async function fetchPhotos(searchPrase, page = 1) {
+  const result = await axios.get("https://api.unsplash.com/search/photos?", {
+    params: {
+      client_id: "BUqEmomTG7T1vLBnVUi4F4sAZAHb-r9RCsxNJ7SWWvI",
+      auto: "format",
+      page: page,
+      per_page: 30,
+      query: searchPrase,
+    },
     headers: {
       "Accept-Version": "v1",
     },
   });
+  return result.data;
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,5 @@
 // import css from "./SearchBar.module.css";
 import fetchPhotos from "../JS/fetchPhotos";
-import responseCheck from "../JS/checkStatus";
 
 export default function SearchBar({ setsState: [setPhotos, setLoading] }) {
   async function handleSubmit(event) {
@@ -13,11 +12,8 @@ export default function SearchBar({ setsState: [setPhotos, setLoading] }) {
     try {
       setLoading(true);
       setPhotos([]);
-      const fetch = await fetchPhotos(searchPrase);
-      const photosArray = await responseCheck(fetch);
-      console.log(photosArray);
+      const photosArray = await fetchPhotos(searchPrase);
       const { total, total_pages, results } = photosArray;
-      // console.log(results)
       setPhotos(results);
     } catch (error) {
       alert(error);
