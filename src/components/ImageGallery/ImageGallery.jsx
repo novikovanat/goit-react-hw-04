67991cd4b99864fa7deeee3510ca69fc8137eb3d
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-04-23 08:37:36 +0200

    typo fixed

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,12 +2,12 @@ import ImageCard from "../ImageCard/ImageCard";
 
 export default function ImageGallery({ photosArray }) {
   console.log(photosArray);
-  const imageList = photosArray.map(({ urls:{small}, alt_description
-    , id }) => (
-    <li key={id}>
-      <ImageCard scr={small} alt={alt_description
-} />
-    </li>
-  ));
+  const imageList = photosArray.map(
+    ({ urls: { small }, alt_description, id }) => (
+      <li key={id}>
+        <ImageCard src={small} alt={alt_description} />
+      </li>
+    )
+  );
   return <ul>{imageList}</ul>;
 }
