b81ef6a416698f745da7e040dcc6775ad5763d62
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-04-24 22:29:18 +0200

    message text changed

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,3 +1,8 @@
 export default function ErrorMessage({ errorText }) {
-  return <p>{errorText}</p>;
+  return (
+    <div>
+      <p>{errorText}</p>
+      <p>Try reload the page</p>
+    </div>
+  );
 }
