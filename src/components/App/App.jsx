80dd9862826391405c6e33e007ae83ce24f53f90
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-04-24 09:20:10 +0200

    modal appears

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,39 @@
 import { useState } from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import ImageGallery from "../ImageGallery/ImageGallery";
+import ImageModal from "../ImageModal/ImageModal";
 import "./App.css";
-import testData from "../../assets/testData.json";
 
 function App() {
+  
   const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  // function openModal() {
+  //   setIsOpen(true);
+  // }
+
+  // function afterOpenModal() {
+  // references are now sync'd and can be accessed.
+  // subtitle.style.color = "#f00";
+  // }
+
+  // function closeModal() {
+  //   setIsOpen(false);
+  // }
 
   return (
     <div>
       <SearchBar setsState={[setPhotos, setLoading]} />
       <ImageGallery photosArray={photos} />
+      <ImageModal
+        isOpen={modalIsOpen}
+        onOpen={setModalIsOpen}
+        // onAfterOpen={afterOpenModal}
+        // onRequestClose={closeModal}
+        // contentLabel="Example Modal"
+      />
     </div>
   );
 }
